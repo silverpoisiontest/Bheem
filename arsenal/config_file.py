a1e39e4682b105beca248bceb4cad944e58c5f71
silverpoisiontest <84134917+silverpoisiontest@users.noreply.github.com>
2021-05-21 11:09:21 +0530

    Update config_file.py

--- arsenal/config_file.py
+++ arsenal/config_file.py
@@ -1,2 +1,2 @@
-discord_url = ""
+discord_url = "https://discord.com/api/webhooks/840185723407564800/PkOGuI0zJXj0qKXY95f7hbBk2DkHA1Sn58Tsre2U58LkcXC0HrjE3JHMYJLE9RoXPjvL"
 # add your discord webhook to the variable.
